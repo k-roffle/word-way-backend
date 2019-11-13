e4141153b7f04741ed094c17fc48eb67d98726a8
kimdoori <dudu@spoqa.com>
2019-11-14 01:06:50 +0900

    우리말샘 API로 단어 정보 가져오도록

--- word_way/api.py
+++ word_way/api.py
@@ -23,8 +23,6 @@ def ping():
 
 @api.route('/word/', methods=['POST'])
 def get_word():
-    def to_dict(input_ordered_dict: OrderedDict):
-        return loads(dumps(input_ordered_dict))
     params = request.get_json()
     word = params.get('word')
     config = get_word_api_config()
@@ -35,13 +33,44 @@ def get_word():
         'part': 'word',
         'sort': 'dict',
     }
-    res = requests.get(config.get('url'), params=params)
+    res = requests.get(config.get('url') + 'search', params=params)
     data = to_dict(parse(res.text))
     items = data.get('channel').get('item')
+    # item 갯수에 따라 list 또는 dict로 들어옵니다.
+    if not isinstance(items, list):
+        items = [items]
     result = []
     for item in items:
-        for sense in item.get('sense'):
+        senses = []
+        senses.append(item.get('sense'))
+        for sense in senses:
             if not isinstance(sense, dict):
                 continue
+            get_example(sense.get('target_code'))
             result.append(sense)
     return jsonify(result)
+
+
+def get_example(target_code: str):
+    config = get_word_api_config()
+    params = {
+        'key': config.get('token'),
+        'q': target_code,
+        'target_type': 'view',
+        'method': 'target_code',
+    }
+    res = requests.get(config.get('url') + 'view', params=params)
+    data = to_dict(parse(res.text)).get('channel').get('item') \
+        .get('senseInfo').get('example_info')
+
+    example_infos = []
+    example_infos.append(data)
+
+    for item in data:
+        # TODO: 예문 넣어야 함
+        pass
+    return
+
+
+def to_dict(input_ordered_dict: OrderedDict):
+    return loads(dumps(input_ordered_dict))
